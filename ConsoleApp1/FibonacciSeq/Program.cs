0b711c53e6e012a4d987530ede2e53f8da948272
Maria Strohanova <mstrohanova@stratadecision.com>
2018-12-07 17:22:15 -0600

    Fibonacci start

---
+++ ConsoleApp1/FibonacciSeq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FibonacciSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+            try
+            {
+                number = Int32.Parse(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(input + " is not a number! Try again! :) ");
+            }
+
+            if (number != 0)
+            {
+                
+            }
+
+            
+            
+        }
+    }
+}
