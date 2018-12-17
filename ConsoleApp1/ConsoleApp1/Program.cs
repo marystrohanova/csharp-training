a9a3f3140dfa94137315b65c12c952a0a6f8eab8
Maria Strohanova <mstrohanova@stratadecision.com>
2018-12-17 17:24:00 -0600

    Print Fibonaccis

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
 
 namespace ConsoleApp1
 {
@@ -33,6 +35,8 @@ namespace ConsoleApp1
                 Console.WriteLine("=> No numbers entered! Please try again =) ");
                 Console.ReadLine();
             }
+
+            doFibonacci();
         }
 
         private static string UserInput()
@@ -68,10 +72,50 @@ namespace ConsoleApp1
 
             List<int> noDupList = new List<int>();
 
-            noDupList =  numbers.Distinct().ToList();
+            noDupList = numbers.Distinct().ToList();
 
             return noDupList;
+        }
 
+        private static void doFibonacci()
+        {
+            int number = 0;
+
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            try
+            {
+                number = Int32.Parse(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(input + " is not a number! Try again! :) ");
+            }
+
+            if (number != 0 || number < 0)
+            {
+
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.WriteLine(Fibonacci(i));
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
         }
     }
 }
--- ConsoleApp1/FibonacciSeq/Program.cs
+++ ConsoleApp1/FibonacciSeq/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FibonacciSeq
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void doFibonacci()
         {
             int number = 0;
+            int[] fibInts = new int[number];
 
             Console.WriteLine("Enter a number: ");
             string input = Console.ReadLine();
@@ -19,13 +21,25 @@ namespace FibonacciSeq
                 Console.WriteLine(input + " is not a number! Try again! :) ");
             }
 
-            if (number != 0)
+            if (number != 0 || number < 0)
             {
-                
+
+                fibInts = new int[number];
+                fibInts[0] = 0;
+                fibInts[1] = 1;
+
+                for (int i = 1; i < number; i++)
+                {
+                    fibInts[number] = fibInts[i] + fibInts[i - 1];
+                    Console.WriteLine(fibInts[number]);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Cannot process sequence. Number is 0 or less then 0");
             }
 
-            
-            
         }
     }
 }
